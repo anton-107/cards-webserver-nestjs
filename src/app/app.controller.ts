014f168cd0471df51cad424ee600b631891a01ac
Anton Nekipelov <226657+anton-107@users.noreply.github.com>
2024-02-09 19:23:44 +0100

    add more routes to play with

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,5 +1,10 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Body, Controller, Get, Post } from "@nestjs/common";
+
+import { AppService } from "./app.service";
+
+interface CreateNameDto {
+  name: string;
+}
 
 @Controller()
 export class AppController {
@@ -8,5 +13,15 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get("/name")
+  getName(): string {
+    return "My name is Nest";
+  }
+
+  @Post("/name")
+  postName(@Body() requestBody: CreateNameDto): string {
+    return `You just posted: ${requestBody.name}`;
   }
 }
