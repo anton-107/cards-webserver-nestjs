71c4d09b3608dcb249a02b9f0ac472a1122441de
Anton Nekipelov <226657+anton-107@users.noreply.github.com>
2024-02-10 19:11:40 +0100

    Generate .d.ts files

---
+++ dist/app/app.controller.d.ts
@@ -0,0 +1,13 @@
+import { AppService } from "./app.service";
+interface CreateNameDto {
+    name: string;
+}
+export declare class AppController {
+    private readonly appService;
+    constructor(appService: AppService);
+    getHello(): string;
+    getName(): string;
+    postName(requestBody: CreateNameDto): string;
+}
+export {};
+//# sourceMappingURL=app.controller.d.ts.map
