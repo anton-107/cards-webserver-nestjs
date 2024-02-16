5e30d234507e0f752f1dd50b8f29012e5b98e3f5
Anton Nekipelov <226657+anton-107@users.noreply.github.com>
2024-02-16 12:10:27 +0100

    mock cards service in the cards controller spec

--- src/cards/cards.controller.spec.ts
+++ src/cards/cards.controller.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from "@nestjs/testing";
+import { instance, mock } from "ts-mockito";
 
 import { CardsController } from "./cards.controller";
+import { CardsService } from "./cards.service";
 
 describe("CardsController", () => {
   let controller: CardsController;
@@ -8,6 +10,12 @@ describe("CardsController", () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CardsController],
+      providers: [
+        {
+          provide: CardsService,
+          useValue: instance(mock(CardsService)),
+        },
+      ],
     }).compile();
 
     controller = module.get<CardsController>(CardsController);
